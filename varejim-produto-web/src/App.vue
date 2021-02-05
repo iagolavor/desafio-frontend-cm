b2d6fc291a91b2c8885304e8e5795b40a01a03ca
Iago Lavor <iago-l@hotmail.com>
2021-02-04 21:39:44 -0300

    Adicionado vuetify e feito layout geral do app

--- varejim-produto-web/src/App.vue
+++ varejim-produto-web/src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="teal lighten-2"
+      dark
+    >
+      <div class="d-flex align-center appbar" style="max-width:1200px;">
+        <v-icon large>mdi-cash</v-icon>
+        <span class="ml-3 text-h4 font-weight-light">O Varejim web</span>
+      </div>
+    </v-app-bar>
+
+    <v-main class="main">
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+.appbar{
+  display: flex;
+  align-items: center;
+  width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.main{
+  background-color: lightgrey;
 }
 </style>
--- varejim-produto-web/src/store/index.js
+++ varejim-produto-web/src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    filters: []
+  },
+  getters: {
+    getFilter: state => {
+      return state.filters
+    }
   },
   mutations: {
+    addFilter: (filters, filter) => {
+      filters.push(filter)
+    },
+    removeFilter: (filters, filter) => {
+      let idx = filters.findIndex(filter)
+      filters = filters.splice(idx, 1)
+    }
   },
   actions: {
   },
